1310fb405728ef9e025c60c602e1f6777c02358f
Peter G.A. Barraud <barraud@adobe.com>
2019-05-08 08:27:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from os import listdir
+from os.path import isfile, join
+import csv
+
+
+def main(dir_path):
+    nse_files = [join(dir_path, f) for f in listdir(dir_path) if isfile(join(dir_path, f)) and f[-7:] != '_F1.txt']
+    highs = []
+    for nse_file  in nse_files:
+        with open(nse_file, 'r') as nse_file_h:
+            for row in csv.reader(nse_file_h):
+                (_, _, _, _, _, high_price, _, total_traded_quantity, _) = row
+                highs.append(high_price)
+            for high in highs:
+                highers = [h for h in highs if h > high]
+                if len(highers) > 0:
+                    highest_high = max(highers)
+                    print(high)
+                    print(highest_high)
+                    print("*" * 50)
+
+
+if __name__ == '__main__':
+    main(r'oneminutedata\2019\02MAY\test')
---
+++ pointtonote.md
@@ -0,0 +1,11 @@
+* F1 => Futures data
+* Intraday file struct (*my current understanding*):
+    * 0 => Symbol
+    * 1 => Date
+    * 2 => Time
+    * 3 => Prev Close
+    * 4 => Open
+    * 5 => High
+    * 6 => Low
+    * 7 => Total Traded Quantity
+    * 8 => (*Pending info*)
